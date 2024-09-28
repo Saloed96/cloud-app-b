418daf7c594c52e5e9f4ad1b20b2962e33c1b561
meleshko.a <meleshko.a@dbbest.com>
2024-09-28 21:31:19 +0200

    init Spring Cloud App B

---
+++ src/main/java/com/saloed/cloud/cloudappb/CloudAppBApplication.java
@@ -0,0 +1,13 @@
+package com.saloed.cloud.cloudappb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAppBApplication.class, args);
+    }
+
+}
